a78131a379284a9aa678033b5c401d4eaf21edce
CJuice <CJuice@users.noreply.github.com>
2019-03-07 08:51:43 -0500

    Update LizardTechJobAnalysis_lidar.py

--- LizardTechJobAnalysis_lidar.py
+++ LizardTechJobAnalysis_lidar.py
@@ -33,31 +33,28 @@ def main():
     output_folder = r'D:\Scripts\GrabLizardTechOutputLogInfo'  # Production
 
     # FUNCTIONS
-    def convert_start_date_time_to_datetime(value):
+    def convert_start_date_time_to_datetime(start_dt_str):
         """
-
-        :param value:
-        :return:
+        Parse string value for start date and time from html table to a datetime object and return object
+        :param start_dt_str: string repre
+        :return: datetime object
         """
-        value = value.strip()
-        if "EDT" in value:
-            # print(f"EDT FLAG {file}")
-            replacement_result = value.replace("EDT", "EST")
+        start_dt_str = start_dt_str.strip()
+        if "EDT" in start_dt_str:
+            replacement_result = start_dt_str.replace("EDT", "EST")
             result = dateutil.parser.parse(replacement_result) - datetime.timedelta(hours=1)
-            # print(f"\t{value} has been converted to EST: {result}")
         else:
-            result = dateutil.parser.parse(value)
-        # print(f"\tRESULT: {result}")
+            result = dateutil.parser.parse(start_dt_str)
         return result
 
-    def count_email_occurrences(df: pd.DataFrame) -> pd.DataFrame:
+    def count_email_occurrences(emails_dataframe: pd.DataFrame) -> pd.DataFrame:
         """
-
-        :param df:
-        :return:
+        Process a dataframe of emails and return the count of occurrences for each unique email
+        :param emails_dataframe: pandas dataframe containing emails from html log files
+        :return: pandas dataframe of unique emails and count of occurrences among processed job log files
         """
         #   count email occurrences
-        email_counts_series = df["Email"].value_counts(normalize=False, sort=True, ascending=True, dropna=False)
+        email_counts_series = emails_dataframe["Email"].value_counts(normalize=False, sort=True, ascending=True, dropna=False)
         email_counts_df = email_counts_series.to_frame()
         email_counts_df.reset_index(inplace=True)
         email_counts_df.rename(columns={"Email": "Count", "index": "Email"}, inplace=True)
@@ -66,22 +63,22 @@ def main():
 
     def create_output_file_path(extension: str) -> str:
         """
-
-        :param extension:
-        :return:
+        Create the output file path string incorporating the date and return string
+        :param extension: file extension to be appended on end of string
+        :return: string to be used in naming output file
         """
         date_string = f"{datetime.datetime.today().year}-{datetime.datetime.today().month}-{datetime.datetime.today().day}"
-        return os.path.join(output_folder, f"LizardTechAnalysis_{date_string}.{extension}")
+        return os.path.join(output_folder, f"LizardTechAnalysis_lidar_{date_string}.{extension}")
 
-    def determine_unique_email_extensions(df: pd.DataFrame) -> pd.DataFrame:
+    def determine_unique_email_extensions(unique_emails_df: pd.DataFrame) -> pd.DataFrame:
         """
-
-        :param df:
-        :return:
+        Count the number of occurrences of unique email extensions such as .gov or .com and return dataframe
+        :param unique_emails_df: dataframe of unique emails from job logs
+        :return: dataframe of unique extensions
         """
         # count email types based on top-level domain values
-        # TODO: Will need to build in protection for addresses that are bogus and don't have '@' or '.'
-        email_parts_series = df["Email"].apply(func=lambda x: x.split("@"))
+        # TODO: May need to build in protection for addresses that are bogus and don't have '@' or '.'
+        email_parts_series = unique_emails_df["Email"].apply(func=lambda x: x.split("@"))
         domain_series = email_parts_series.apply(func=lambda x: x[-1])
         top_level_domain_series = domain_series.apply(func=lambda x: x.split(".")[-1])
         unique_values = top_level_domain_series.value_counts()
@@ -89,13 +86,13 @@ def main():
         unique_values_df.rename(columns={"index": "TopLevelDomain", "Email": "Count"}, inplace=True)
         return unique_values_df
 
-    def extract_email_series_from_messages(df: pd.DataFrame) -> pd.Series:
+    def extract_email_series_from_messages(html_table_df: pd.DataFrame) -> pd.Series:
         """
-
-        :param df:
-        :return:
+        Extract pandas series of email specific messages from the html job log table Messages column
+        :param html_table_df: dataframe of entire html table contents
+        :return: pandas series of email specific messages
         """
-        df_no_na = df.dropna()
+        df_no_na = html_table_df.dropna()
         df_no_na = df_no_na[df_no_na["Message"].str.contains("@")]
         try:
             # NOTE: re.findall returns a list and I only observed one email per list in my test data
@@ -108,13 +105,13 @@ def main():
         else:
             return emails_series
 
-    def extract_issuing_url_series(df: pd.DataFrame) -> pd.Series:
+    def extract_issuing_url_series(html_table_df: pd.DataFrame) -> pd.Series:
         """
         Examine the Messages column, identifying the 'Issuing URL: ' records, extract url, return a Series
-        :param df:
-        :return:
+        :param html_table_df: dataframe of entire html table contents
+        :return: pandas series of Message content containing Issuing URL
         """
-        df_no_na = df.dropna()
+        df_no_na = html_table_df.dropna()
         messages_df = df_no_na[["Message"]]
         messages_df = messages_df[messages_df["Message"].str.startswith("Issuing URL: ")]
         try:
@@ -128,9 +125,9 @@ def main():
 
     def extract_job_start_date_time_line(file_path: str) -> str:
         """
-
-        :param file_path:
-        :return:
+        Open log file and extract the start time value from html content and return
+        :param file_path: path to job log html file
+        :return: string
         """
         keyphrase = "Log session start time"
         break_tag = "<br>"
@@ -146,25 +143,23 @@ def main():
                     continue
         return "NaN"
 
-    def extract_query_string_dicts(series: pd.Series) -> pd.Series:
+    def extract_query_string_dicts(issuing_url_ser: pd.Series) -> pd.Series:
         """
         Parse the query string parameters and values from a Series of url values and return results as a dictionary
-        :param series:
-        :return:
+        :param issuing_url_ser: series of query string values parsed from issuing urls
+        :return: series of query parameters and values
         """
-        query_string_dicts_series = (series.apply(func=lambda x: urlpar.parse_qs(qs=urlpar.urlparse(x).query)))
-        # .reset_index(drop=True))
+        query_string_dicts_series = (issuing_url_ser.apply(func=lambda x: urlpar.parse_qs(qs=urlpar.urlparse(x).query)))
         return query_string_dicts_series
 
-    def process_level_summary_by_job(df: pd.DataFrame) -> list:
+    def process_level_summary_by_job(html_table_df: pd.DataFrame) -> list:
         """
-
-        :param df:
-        :return:
+        Extract the Level information from the html table and summarize value counts for types present, returning list
+        :param html_table_df: dataframe of entire html table contents
+        :return: list of dataframes for each job
         """
         level_summary_ls = []
-        level_groupeddf = df.groupby([df.index])
-
+        level_groupeddf = html_table_df.groupby([html_table_df.index])
         for name, group in level_groupeddf:
             level_df = group["Level"].value_counts().to_frame()
             level_df["JOB_ID"] = name
@@ -175,19 +170,19 @@ def main():
     def setup_initial_dataframe(file_path: str) -> pd.DataFrame:
         """
         Process html file, containing a single table, into a dataframe with the same columns as the table
-        :param file_path:
-        :return:
+        :param file_path: path to the html job log file to be examined
+        :return: dataframe of html table content
         """
         # Use pandas to create list of dataframes from tables in html. Should only be one per file so get zero index.
-        df = pd.read_html(io=file_path)[0]
+        job_log_file_html_table_contents_df = pd.read_html(io=file_path)[0]
 
         # For unknown reason, true table headers are in row 0. Grab them, use to rename columns, drop first row
         #   Columns are given a 0 through 4 index instead of the header value. Need dict to rename them.
-        column_names_series = df.iloc[0]
+        column_names_series = job_log_file_html_table_contents_df.iloc[0]
         column_rename_dict = dict(zip(list(range(0, len(column_names_series))), column_names_series))
-        df.rename(columns=column_rename_dict, inplace=True)
-        df.drop([0], axis=0, inplace=True)  # drop first row, which is the headers
-        return df
+        job_log_file_html_table_contents_df.rename(columns=column_rename_dict, inplace=True)
+        job_log_file_html_table_contents_df.drop([0], axis=0, inplace=True)  # drop first row, which is the headers
+        return job_log_file_html_table_contents_df
 
     # FUNCTIONALITY
     #   Need two lists for storing dataframe from each/every file being inspected. Lists will then become a master df
@@ -208,7 +203,7 @@ def main():
 
                 # Extract values such as job start date and time
                 start_dt_string = extract_job_start_date_time_line(file_path=full_file_path)
-                start_dtobj_UTC = convert_start_date_time_to_datetime(value=start_dt_string)
+                start_dtobj_UTC = convert_start_date_time_to_datetime(start_dt_str=start_dt_string)
                 # from_zone = dateutil.tz.tzutc()
                 to_zone = dateutil.tz.tzlocal()
                 start_dtobj_UTC.replace(tzinfo=to_zone)  # Not sure how will be affected by time changes on my puter
@@ -253,7 +248,7 @@ def main():
 
     # ___________________________
     #   LEVEL SUMMARY (INFO, ERROR)
-    level_summary_list = process_level_summary_by_job(df=master_html_values_df)
+    level_summary_list = process_level_summary_by_job(html_table_df=master_html_values_df)
     master_level_df = pd.DataFrame(pd.concat(objs=level_summary_list))
     master_level_df.reset_index(drop=False, inplace=True)
     master_level_df.rename(columns={"index": "Level", "Level": "Count"}, inplace=True)
@@ -262,27 +257,26 @@ def main():
     # ___________________________
     #   EMAIL PROCESSING
     #   isolate the html file Message values that contain an '@'
-    emails_df = (extract_email_series_from_messages(df=master_html_values_df)
+    emails_df = (extract_email_series_from_messages(html_table_df=master_html_values_df)
                  .to_frame(name="Email")
                  .reset_index())
 
     #   process email occurrences
-    email_counts_df = count_email_occurrences(df=emails_df)
+    email_counts_df = count_email_occurrences(emails_dataframe=emails_df)
 
     #   process emails for the unique extensions (gov, com, edu, etc) that occur
-    unique_email_extensions_df = determine_unique_email_extensions(df=email_counts_df)
+    unique_email_extensions_df = determine_unique_email_extensions(unique_emails_df=email_counts_df)
 
     # ___________________________
     #   ISSUING URL PROCESSING
     #   Issuing url query string value extraction
-    issuing_url_series = extract_issuing_url_series(df=master_html_values_df)  # This series contains a job id index
+    issuing_url_series = extract_issuing_url_series(html_table_df=master_html_values_df)  # This series contains a job id index
     issuing_url_query_string_dicts_series = extract_query_string_dicts(issuing_url_series)
 
     # ___________________________
     # QUERY PARAMETER EXAMINATION - MULTIPLE OUTPUTS GENERATED
     # Iterate over the query parameters in the issuing url's in the html logs, simmer down to unique occurrences
     #   by job, then get the overall number of times (number of unique jobs) that a value was used/requested by a user
-
     # NOTE: Changing the order of the dictionary values will change the order of the excel tabs
     query_parameter_explanation = {"cat": "Catalog",
                                    "thinningFactor": "Thinning Factor",
@@ -294,7 +288,6 @@ def main():
                                    "bounds": "Exporting Extent",
                                    "item": "Unknown Meaning",
                                    }
-
     query_parameter_values_df_dict = {}
 
     # Create a dataframe for each query parameter and store in dictionary with explanation term as key
@@ -312,25 +305,18 @@ def main():
     #   Must get unique occurrence for each job, otherwise counts influenced by quantity of issuing url requests
     # all_jobs_df = master_html_values_df.index.unique().to_frame(index=False)  # TURNED OFF
 
-    def detect_null_return_substitute(val):
+    def isolate_value_in_list_or_replace_null(attr_val):
         """
         Detect numpy NaN values, type float when inspect, and replace with value.
         This was added so that null/empty query parameters can be included in counts, which is important for indicating
         how many jobs did not define the parameter.
-        :param val:
-        :return:
+        :param attr_val: string value to be substituted for null value
+        :return: value inside of list or a string substitute for np.NaN value
         """
 
-        if type(val) is list:
-            # string = ""
-            # for item in val:
-                # if string == "":
-                #     string = item
-                # else:
-                #     string += f", {item}"
-            # return string
-            return val[0]
-        elif np.isnan(val):
+        if type(attr_val) is list:
+            return attr_val[0]
+        elif np.isnan(attr_val):
             return "DoIT Detected NULL"
         else:
             return "Unknown Value"
@@ -339,26 +325,17 @@ def main():
     query_param_unique_dfs_dict = {}
     for key, value in query_parameter_values_df_dict.items():
         query_param_values_df = value.to_frame(name=key)
-        query_param_values_df[key] = query_param_values_df[key].apply(detect_null_return_substitute)
+        query_param_values_df[key] = query_param_values_df[key].apply(isolate_value_in_list_or_replace_null)
         # query_param_values_df[key] = query_param_values_df[key].apply(lambda x: x[0]) # Replaced by custom function
         query_param_values_df.rename(columns={"Message": key}, inplace=True)
         query_param_values_df[key] = query_param_values_df[key].apply(lambda x: tuple([x]))  # pd.unique() won't work on lists, unhashable, cast to tuple
         unique_gb = query_param_values_df.groupby("JOB_ID")
         unique_results_df = unique_gb[key].unique().to_frame()
         unique_results_by_job_dict[key] = unique_results_df
-        # if key == "Catalog":
-        #     t = unique_results_df.copy()
-        #     t["length"] = t["Catalog"].apply(lambda x: len(x))
-        #     print(t[(t["length"] > 1)])
 
         # all_jobs_df = all_jobs_df.join(other=unique_results_df, on="JOB_ID", how="left")  # TURNED OFF
 
         list_of_unique_tuples = unique_results_df[key].tolist()
-        # if key == "Catalog":
-        #     for item in list_of_unique_tuples:
-        #         if len(item) > 1:
-        #             print(item)
-
         unique_tuples_list = []
         for item in list_of_unique_tuples:
             # NOTE: Items are np.ndarray. Encountered issues in value_counts() with ndarray's. Needed them as lists.
@@ -387,11 +364,6 @@ def main():
         # Final conversion of values to strings so that print out to excel doesn't show tuple container symbols
         query_param_unique_dfs_dict[key].reset_index(inplace=True)
         query_param_unique_dfs_dict[key][key] = query_param_unique_dfs_dict[key][key].apply(lambda x: x[0])
-
-    # TESTING: Expose multi-catalog records
-    # t = unique_results_by_job_dict["Catalog"]
-    # t["length"] = t["Catalog"].apply(lambda x: len(x))
-    # print(t[(t["length"] > 1)])
 
     # ___________________________
     # SPATIAL EXAMINATION OF EXPORT EXTENT
@@ -437,7 +409,9 @@ def main():
     # # Add these to a feature class with date.
     #
     # exit()
-    pass
+
+    pass    # For above and below pycharm comments to be separate and foldable
+
     # ___________________________
     # DATE RANGE EVALUATION
     date_range_df = pd.DataFrame(data=[[np.min(date_range_list), np.max(date_range_list)]],
@@ -487,7 +461,7 @@ def main():
         #                      header=True,
         #                      index=False)
 
-        print("Process Complete")
+        print(f"Process Complete. See output file {create_output_file_path(extension='xlsx')}")
 
 
 if __name__ == "__main__":
